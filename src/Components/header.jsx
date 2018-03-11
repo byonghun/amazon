889237b0b6cc8bbf7a602d611e1f5435b805cd6b
byonghun <john.bh.kwak@gmail.com>
2018-03-10 21:36:04 -0800

    Add header

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -4,22 +4,62 @@ import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
 import IconButton from 'material-ui/IconButton'
 import MenuIcon from 'material-ui-icons/Menu'
+import AccountCircle from 'material-ui-icons/AccountCircle'
+import Menu, { MenuItem } from 'material-ui/Menu'
 
 export default class extends Component {
-	render() {
-		return (
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton color="blue" aria-label="Menu">
-						{/* Company Logo */}
-						<MenuIcon /> 
-					</IconButton>
-					<Typography variant="title" color="inherit">
-					  {/* Company name */}
-					  Title
-					</Typography>
-				</Toolbar>
-			</AppBar>
-		)
-	}
+  state = { anchorEl: null, open: false }
+  render() {
+    const { anchorEl } = this.state
+    return (
+      <div style={styles.display}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Menu">
+              {/* Company Logo */}
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" style={styles.flex}>
+              {/* Company name */}
+              Title
+            </Typography>
+            {/* When authenticated show profile */}
+            <IconButton
+              aria-owns='menu-appbar'
+              aria-haspopup="true"
+              onClick={() => console.log('clicked')}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              anchorEl={anchorEl}
+              open={anchorEl}
+              onClose={() => console.log('close')} >
+              <MenuItem onClick={() => console.log('menu')}>Profile</MenuItem>
+              <MenuItem onClick={() => console.log('account')}>My Account</MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
+}
+
+const styles = {
+  display: {
+    display: 'flex'
+  },
+  flex: {
+    flex: 1
+  }
 }
